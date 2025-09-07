1a8b26a2bba3e43a035b959b98715189a22e1975
Matusalem M. Lanes <mmlanes@gmail.com>
2025-09-06 22:34:08 -0300

    A interrupção do encoder parou de funcionar com a opção -flto em build_flags no Platformio.ini. Ao desabilitar fazendo: ;    -flto O uso de flash sobe de 78% para 84,9%. O LCD e o encoder estão funcionando.

--- lib/STM32/EncoderSTM32.h
+++ lib/STM32/EncoderSTM32.h
@@ -13,9 +13,9 @@ class EncoderSTM32
     Variavel<bool>& reverso_;
     Variavel<int32_t>& posicao_;
     Variavel<uint32_t>& posicaoMaxima_;
+
     void atualizarPosicaoIsr(void)
     {
-        Serial2.println("ISR Encoder");
         bool a = digitalRead(pinA_);
         bool b = digitalRead(pinB_);
         if (a == b)
@@ -35,7 +35,8 @@ class EncoderSTM32
     }
 
 public:
-    EncoderSTM32(Variavel<int32_t>& pos, Variavel<uint32_t>& max, Variavel<bool>& rev, uint8_t pinA, uint8_t pinB, bool pinA_is_Isr = true, bool pinB_is_Isr = false, bool contagemReversa = false)
+    EncoderSTM32(Variavel<int32_t>& pos, Variavel<uint32_t>& max, Variavel<bool>& rev, uint8_t pinA, uint8_t pinB, 
+                    bool pinA_is_Isr = true, bool pinB_is_Isr = false, bool contagemReversa = false)
     : posicao_(pos), posicaoMaxima_(max), reverso_(rev), pinA_(pinA), pinB_(pinB), pinA_is_Isr_(pinA_is_Isr), pinB_is_Isr_(pinB_is_Isr)
     {
         pinMode(pinA_, INPUT_PULLUP);
